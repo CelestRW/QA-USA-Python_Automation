a52fee96d374861c7aa6618b28fd94e71688253a
Celest Ward <celest.rios@outlook.com>
2025-06-10 15:10:23 -0500

    Add placeholder test function to main.py for Project 8

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-
+import data
 class TestUrbanRoutes:
+    def test_set_route(self):
+        #Add in S8
+        print("Function created for set route")
+        pass
+    def test_select_plan(self):
+        #Add in S8
+        print("Function created for select plan")
+        pass
+    def test_fill_phone_number(self):
+        #Add in S8
+        print("Function created for fill phone number")
+        pass
+    def test_fill_card(self):
+        #Add in S8
+        print("Function created for fill card")
+        pass
+    def test_comment_for_driver(self):
+        #Add in S8
+        print("Function created for comment for driver")
+        pass
+    def test_order_blanket_and_handkerchiefs(self):
+        #Add in S8
+        print("FUnction created for ordering blanket and handkerchiefs")
+        pass
+    def test_order_2_ice_cream(self):
+        #Add in S8
+        print("Function created for ordering 2 ice creams")
+        pass
+    def test_car_search_model_appears(self):
+        #Add in S8
+        print("Function created for checking car model appears")
+        pass
